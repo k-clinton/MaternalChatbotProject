59245b36fd24260537fc9d111d9c1e3eaa43cbf3
Clinton Omondi <kingclinn40931@gmail.com>
2026-03-18 08:09:46 +0300

    feat: Set up TypeORM data source

---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const databaseConfig: DataSourceOptions = {
+  type: 'sqlite',
+  database: 'maternal_health.sqlite',
+  // Path adjustment: Since we run this from src or dist, use relative path or __dirname carefully
+  entities: [__dirname + '/../models/**/*.ts'], // Changed to match the models folder
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: false,
+};
+
+const dataSource = new DataSource(databaseConfig);
+
+export default dataSource;
+
